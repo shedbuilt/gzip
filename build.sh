53b7758525d9fc0e3b075d7a5ccaf2a8f0da2826
Auston Stewart <auston@shedbuilt.net>
2018-01-12 01:57:28 -0500

    Updating to 1.9

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-./configure --prefix=/usr
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-mkdir -v ${SHED_FAKEROOT}/bin
-mv -v ${SHED_FAKEROOT}/usr/bin/gzip ${SHED_FAKEROOT}/bin
+case "$SHED_BUILDMODE" in
+    toolchain)
+        SHEDPKG_PREFIX='/tools'
+        ;;
+    *)
+        SHEDPKG_PREFIX='/usr'
+        ;;
+esac
+
+./configure --prefix=$SHEDPKG_PREFIX || return 1
+make -j $SHED_NUMJOBS || return 1
+make DESTDIR="$SHED_FAKEROOT" install || return 1
+
+if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
+    mkdir -v "${SHED_FAKEROOT}/bin"
+    mv -v "${SHED_FAKEROOT}/usr/bin/gzip" "${SHED_FAKEROOT}/bin"
+fi
